257ed368f9942072b9dd81dabff910f7c0d39432
Ádám <zorgadam@gmail.com>
2021-05-29 15:18:24 +0200

    Ip cím kiírása fájlba

--- create_server.sh
+++ create_server.sh
@@ -6,19 +6,21 @@ mkdir mc_server
 cd mc_server
 curl  https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar -o server.jar
 echo "java -Xmx2048M -Xms2048M -jar server.jar" > start.bat
-
+ip=$(curl -s https://api.ipify.org)
+echo "A szerverhez ezen az ip címen lehet csatlakozni: $ip" > olvass_el.txt
 bash start.bat
 
 clear
 
 read -n 1 -s -r -p "Nyomj meg egy gombot a folytatáshoz. Ezzel elfogadod a felhasználási feltételeket. (https://account.mojang.com/documents/minecraft_eula)"
+
 clear
+
 sed 's/false/true/' eula.txt > eula_tmp.txt; mv eula_tmp.txt eula.txt
-
 sleep 1
-
 sed 's/online-mode=true/online-mode=false/' server.properties > prop_tmp.properties; mv prop_tmp.properties server.properties
-
 sleep 1
+
 
 bash start.bat
+
